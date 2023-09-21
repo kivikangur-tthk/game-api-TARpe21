da85a57dccb19f10e347dff093bbce1e65629adf
kristjankivikangur <kristjan.kivikangur@tthk.ee>
2023-09-21 12:47:59 +0300

    Add yaml and jwt authentication

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const app = require("express")()
 const port = 8080
 const swaggerui = require("swagger-ui-express")
-const swaggerDocument = require("./docs/swagger.json")
+const yamljs = require("yamljs")
+const swaggerDocument = yamljs.load("./docs/swagger.yaml")
 
 app.use("/docs", swaggerui.serve, swaggerui.setup(swaggerDocument))
 
 app.get("/games", (req, res) => {
+    console.log(JSON.stringify(req.headers));
+    console.log(JSON.stringify(req.cookies))
     res.send([
         { id: 1, name: "Witcher 3" },
         { id: 2, name: "Cyberpunk 2077" }
