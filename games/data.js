3e3aba35171447500997e0d5f62834f30e749a1a
kristjankivikangur <kristjan.kivikangur@tthk.ee>
2023-09-26 15:30:42 +0300

    Separate data and routing

---
+++ games/data.js
@@ -0,0 +1,11 @@
+let data = [
+    { id: 1, name: "Witcher 3", price: 29.99 },
+    { id: 2, name: "Cyberpunk 2077", price: 59.99 },
+    { id: 3, name: "Minecraft", price: 26.99 },
+    { id: 4, name: "Counter-Strike: Global Offencive", price: 0 },
+    { id: 5, name: "Roblox", price: 0 }
+]
+
+exports.getAll = () => {
+    return data
+}
--- index.js
+++ index.js
@@ -3,16 +3,13 @@ const port = 8080
 const swaggerui = require("swagger-ui-express")
 const yamljs = require("yamljs")
 const swaggerDocument = yamljs.load("./docs/swagger.yaml")
+const games = require("./games/data")
 
 app.use("/docs", swaggerui.serve, swaggerui.setup(swaggerDocument))
 
 app.get("/games", (req, res) => {
-    console.log(JSON.stringify(req.headers));
-    console.log(JSON.stringify(req.cookies))
-    res.send([
-        { id: 1, name: "Witcher 3" },
-        { id: 2, name: "Cyberpunk 2077" }
-    ])
+    res.send(games.getAll())
+})
 })
 
 app.listen(port, () => {
