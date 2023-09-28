7b9f3d3ea71ca85377f5be8e93d78f6554c36637
kristjankivikangur <kristjan.kivikangur@tthk.ee>
2023-09-28 09:53:56 +0300

    Add players get all and get by id

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const swaggerui = require("swagger-ui-express")
 const yamljs = require("yamljs")
 const swaggerDocument = yamljs.load("./docs/swagger.yaml")
 const games = require("./games/data")
+const players = require("./players/data")
 
 app.use("/docs", swaggerui.serve, swaggerui.setup(swaggerDocument))
 
@@ -17,6 +18,17 @@ app.get("/games/:id", (req, res) => {
         return res.status(404).send({ error: `Game not found` })
     }
     res.send(foundGame)
+})
+app.get("/players", (req, res) => {
+    res.send(players.getAll())
+})
+
+app.get("/players/:id", (req, res) => {
+    const foundPlayer = players.getById(req.params.id)
+    if (foundPlayer === undefined) {
+        return res.status(404).send({ error: `Player not found` })
+    }
+    res.send(foundPlayer)
 })
 
 app.listen(port, () => {
