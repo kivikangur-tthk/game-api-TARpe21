980b601fcfdac8beb947616aab3aeedf236051cc
kristjankivikangur <kristjan.kivikangur@tthk.ee>
2023-10-12 10:08:20 +0300

    Add ORM for database connection

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-const app = require("express")()
-const port = 8080
+require("dotenv").config()
+const express = require("express")
+const app = express()
+const port = process.env.PORT
 const swaggerui = require("swagger-ui-express")
-const swaggerDocument = require("./docs/swagger.json")
+const yamljs = require("yamljs")
+const swaggerDocument = yamljs.load("./docs/swagger.yaml")
+const { Sequelize } = require("sequelize")
+const sequelize = new Sequelize(process.env.DATABASE, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
+    dialect: "mariadb"
+})
+try {
+    sequelize.authenticate().then(() => {
+        console.log('Connection has been established successfully.');
+    });
+} catch (error) {
+    console.error('Unable to connect to the database:', error);
+}
 
+app.use(express.json())
 app.use("/docs", swaggerui.serve, swaggerui.setup(swaggerDocument))
 
 app.listen(port, () => {
